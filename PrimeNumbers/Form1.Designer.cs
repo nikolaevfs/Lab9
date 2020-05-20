8645c4c6d099c2b91e66ce916d03bd494b4e538f
UncleFedor527 <56272782+UncleFedor527@users.noreply.github.com>
2020-05-20 11:47:20 +0300

    Добавьте файлы проекта.

---
+++ PrimeNumbers/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace PrimeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Диапазон чисел:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "от";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "до";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Количество потоков:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(506, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вывод простых чисел из диапозона";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox4.Location = new System.Drawing.Point(0, 259);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(533, 191);
+            this.textBox4.TabIndex = 8;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(15, 159);
+            this.trackBar1.Maximum = 4;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(191, 56);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.Value = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "1";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(190, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PrimeNumbers/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PrimeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Range
+        {
+            public int from, to;
+
+            public Range() { }
+
+            public Range(int from, int to)
+            {
+                this.from = from;
+                this.to = to;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "";
+            int parts = trackBar1.Value;
+            uint from, to;
+
+            try
+            {
+                from = Convert.ToUInt32(textBox1.Text);
+                to = Convert.ToUInt32(textBox2.Text);
+                if (from > to)
+                {
+                    throw new Exception("Лицам до 18 регистрация запрещена");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Входные данные - положительные, целые числа");
+                
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Входные данные - положительные, целые числа");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный диапозон");
+            }
+
+            switch (parts)
+            {
+                case 1:
+                    Range nor = new Range();
+                    nor.from = Convert.ToInt32(textBox1.Text);
+                    nor.to = Convert.ToInt32(textBox2.Text);
+                    Thread thread1 = new Thread(new ParameterizedThreadStart(prime));
+                    thread1.Start(nor);
+                    break;
+                case 2:
+                    Range nor21 = new Range();
+                    nor21.from = Convert.ToInt32(textBox1.Text);
+                    nor21.to = Convert.ToInt32(textBox2.Text)/2;
+                    Thread thread2 = new Thread(new ParameterizedThreadStart(prime));
+                    thread2.Start(nor21);
+
+                    Range nor22 = new Range();
+                    nor22.from = Convert.ToInt32(textBox2.Text) / 2 + 1;
+                    nor22.to = Convert.ToInt32(textBox2.Text);
+                    Thread thread22 = new Thread(new ParameterizedThreadStart(prime));
+                    thread22.Start(nor22);
+                    break;
+                case 3:
+                    Range nor31 = new Range();
+                    nor31.from = Convert.ToInt32(textBox1.Text);
+                    nor31.to = Convert.ToInt32(textBox2.Text) / 3;
+                    Thread thread3 = new Thread(new ParameterizedThreadStart(prime));
+                    thread3.Start(nor31);
+
+                    Range nor32 = new Range();
+                    nor32.from = Convert.ToInt32(textBox2.Text) / 3 + 1;
+                    nor32.to = Convert.ToInt32(textBox2.Text) / 3 * 2;
+                    Thread thread33 = new Thread(new ParameterizedThreadStart(prime));
+                    thread33.Start(nor32);
+
+                    Range nor33 = new Range();
+                    nor33.from = Convert.ToInt32(textBox2.Text) / 3 * 2 + 1;
+                    nor33.to = Convert.ToInt32(textBox2.Text);
+                    Thread thread333 = new Thread(new ParameterizedThreadStart(prime));
+                    thread333.Start(nor33);
+                    break;
+                case 4:
+                    Range nor41 = new Range();
+                    nor41.from = Convert.ToInt32(textBox1.Text);
+                    nor41.to = Convert.ToInt32(textBox2.Text) / 4;
+                    Thread thread4 = new Thread(new ParameterizedThreadStart(prime));
+                    thread4.Start(nor41);
+
+                    Range nor42 = new Range();
+                    nor42.from = Convert.ToInt32(textBox2.Text) / 4 + 1;
+                    nor42.to = Convert.ToInt32(textBox2.Text) / 4 * 2;
+                    Thread thread44 = new Thread(new ParameterizedThreadStart(prime));
+                    thread44.Start(nor42);
+
+                    Range nor43 = new Range();
+                    nor43.from = Convert.ToInt32(textBox2.Text) / 4 * 2 + 1;
+                    nor43.to = Convert.ToInt32(textBox2.Text) / 4 * 3;
+                    Thread thread444 = new Thread(new ParameterizedThreadStart(prime));
+                    thread444.Start(nor43);
+
+                    Range nor44 = new Range();
+                    nor44.from = Convert.ToInt32(textBox2.Text) / 4 * 3 + 1;
+                    nor44.to = Convert.ToInt32(textBox2.Text);
+                    Thread thread4444 = new Thread(new ParameterizedThreadStart(prime));
+                    thread4444.Start(nor44);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void prime(object x)
+        {
+            int from = (x as Range).from;
+            int to= (x as Range).to; 
+
+            for (int i = from ; i <= to; i++)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    textBox4.Invoke(
+                (ThreadStart)delegate ()
+                {
+                    textBox4.Text += i + " ";
+                });
+                }
+            }
+        }
+
+        public static bool IsPrimeNumber(int n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
